ce4f4b76110b9d2e652a7ed855b435cbf148fa56
Ariel <carlosacapozucca@hotmail.com>
2022-05-21 17:37:17 -0300

    Avance en develop

---
+++ src/test/java/ar/edu/cuvl/model/CasoPruebaCincoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.cuvl.model;
+
+import ar.edu.cuvl.model.type.Limpieza;
+import org.junit.*;
+
+import java.util.List;
+
+public class CasoPruebaCincoTest {
+
+    List<TipoSuperficie> tipoSuperficieList = new
+
+    Pedido pedido = new Pedido();
+
+}
