edc19a3ac0a55d836ce0e550e08d9bd698237d81
jgabrielc84 <jgabrielc84@gmail.com>
2022-05-21 16:24:03 -0300

    -Actualizacion de CLases y Diagramas

--- src/main/java/ar/edu/cuvl/model/Empresa.java
+++ src/main/java/ar/edu/cuvl/model/Empresa.java
@@ -3,7 +3,6 @@ package ar.edu.cuvl.model;
 import ar.edu.cuvl.controller.AdministradorClientes;
 import ar.edu.cuvl.controller.AdministradorPedidos;
 import ar.edu.cuvl.controller.AdministradorRobots;
-import ar.edu.cuvl.exception.PedidoInvalidoException;
 import ar.edu.cuvl.model.type.Limpieza;
 import ar.edu.cuvl.model.type.Servicio;
 import ar.edu.cuvl.model.type.Superficie;
@@ -13,63 +12,49 @@ import java.util.ArrayList;
 
 public class Empresa {
 
-    private AdministradorRobots administradorRobots;
-    private AdministradorClientes administradorClientes;
-    private AdministradorPedidos administradorPedidos;
-    private ValidadorPedido validadorPedido;
-    private String message;
-    public Empresa() {
-        this.administradorRobots = new AdministradorRobots();
-        this.administradorClientes = new AdministradorClientes();
-        this.administradorPedidos = new AdministradorPedidos();
-        this.validadorPedido = new ValidadorPedido();
-    }
+    public static void main(String[] args) {
 
-//    public void validarPedido(Pedido pedido) throws PedidoInvalidoException{
-//
-//        try {
-//            this.validadorPedido.validarEsPedido(pedido);
-//            System.out.println("Validacion Pedido: OK");
-//        } catch (PedidoInvalidoException e) {
-//            //administradorPedidos.getPedidosRechazados().add(pedido);
-//            throw new PedidoInvalidoException(e.getMessage());
-//        } catch (Exception e) {
-//            //e.printStackTrace();
-//            //throw new PedidoInvalidoException(e.getMessage());
-//        }
-//    }
+        AdministradorRobots administradorRobots = new AdministradorRobots();
+        AdministradorClientes administradorClientes = new AdministradorClientes();
+        AdministradorPedidos administradorPedidos = new AdministradorPedidos();
+        ValidadorPedido validadorPedido = new ValidadorPedido();
 
-    public String ingresarPedido(Pedido pedido) {
+        //Crear Clientes en AdministradorClientes
+        TipoServicio tipoServicio = new Economic();
+        Cliente cliente1 = new Cliente(11111111,tipoServicio);
+        administradorClientes.ingresarCliente(cliente1);
+
+
+        //Prueba Caso 1
+        //Datos de Pedido
+        Cliente cliente = administradorClientes.buscarCliente(1);
+        boolean ordenamiento = true;
+        ArrayList <Superficie > tipoSuperficies = new ArrayList<>();
+        tipoSuperficies.add(Superficie.PISO);
+        tipoSuperficies.add(Superficie.MUEBLE);
+        String direccion = "Cordoba 1501";
+
+        //Creo
+        Pedido pedido = new Pedido(1, cliente.getDni(), direccion, Limpieza.COMPLEJA, ordenamiento, tipoSuperficies, cliente, );
+        //public Pedido(int numeroPedido, int numeroCliente, String direccion, TipoLimpieza tipoLimpieza, boolean ordenamiento, ArrayList<TipoSuperficie> superficies) {
+
 
         try {
-            this.administradorPedidos.ingresarPedido(pedido,administradorRobots);
+            validarPedido(Pedido);
+            this.administradorPedidos.ingresarPedido(pedido);
         } catch (Exception e) {
-            message=e.getMessage();
-        }
-        return message;
-    }
-
-    public void ingresarCliente(Cliente cliente) {
-        try {
-            administradorClientes.ingresarCliente(cliente);
-        } catch (Exception e) { //TODO atrapar cualquier error que tire el administrador de cliente al crear Cliente
             e.printStackTrace();
         }
+
     }
 
-    public AdministradorRobots getAdministradorRobots() {
-        return administradorRobots;
-    }
+    public void validarPedido(Pedido pedido){
 
-    public AdministradorClientes getAdministradorClientes() {
-        return administradorClientes;
-    }
+        try{
+            this.validadorPedido.validarEsPedido(pedido);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    public AdministradorPedidos getAdministradorPedidos() {
-        return administradorPedidos;
-    }
-
-    public ValidadorPedido getValidadorPedido() {
-        return validadorPedido;
     }
 }
