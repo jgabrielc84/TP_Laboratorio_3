2811022a4f66a57ddc77571b19d8b1e42376eeb6
jgabrielc84 <jgabrielc84@gmail.com>
2022-05-21 16:24:03 -0300

    -Actualizacion de CLases y Diagramas

--- src/main/java/ar/edu/cuvl/Main.java
+++ src/main/java/ar/edu/cuvl/Main.java
@@ -2,6 +2,8 @@ package ar.edu.cuvl;
 
 import ar.edu.cuvl.model.*;
 import ar.edu.cuvl.model.robot.K311YA;
+import ar.edu.cuvl.model.type.Limpieza;
+import ar.edu.cuvl.model.type.Superficie;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -10,6 +12,35 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Empresa empresa = new Empresa();
+
+        //Crear Clientes en AdministradorClientes
+        TipoServicio tipoServicio = new Economic();
+        Cliente cliente1 = new Cliente(11111111,tipoServicio);
+
+        empresa.ingresarCliente(cliente1);
+
+
+        //Prueba Caso 1
+        //Datos de Pedido
+        Cliente cliente = administradorClientes.buscarCliente(1);
+        boolean ordenamiento = true;
+        ArrayList <Superficie> tipoSuperficies = new ArrayList<>();
+        tipoSuperficies.add(Superficie.PISO);
+        tipoSuperficies.add(Superficie.MUEBLE);
+        String direccion = "Cordoba 1501";
+
+        //Creo
+        Pedido pedido = new Pedido(1, cliente.getDni(), direccion, Limpieza.COMPLEJA, ordenamiento, tipoSuperficies, cliente, );
+        //public Pedido(int numeroPedido, int numeroCliente, String direccion, TipoLimpieza tipoLimpieza, boolean ordenamiento, ArrayList<TipoSuperficie> superficies) {
+
+
+        try {
+            empresa.validarPedido(Pedido);
+            empresa.administradorPedidos.ingresarPedido(pedido);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
--- src/main/java/ar/edu/cuvl/model/AsignadorRobot.java
+++ src/main/java/ar/edu/cuvl/model/AsignadorRobot.java
@@ -7,16 +7,32 @@ import java.util.ArrayList;
 public class AsignadorRobot {
 
 
-    public ArrayList<Robot> armameLaListaDeRobotPorFa(Pedido pedido){
-        ArrayList<Robot> robots;
+
+    public ArrayList<Robot> armameLaListaDeRobotPorFa(Pedido pedido, ArrayList<Robot> robots){
+
+        ArrayList<Robot> robotsAsignados = new ArrayList<>();
+        ArrayList<Robot> robotsLimpieza = new ArrayList<>();
+        ArrayList<Robot> robotsOrdenamiento = new ArrayList<>();
 
         //TODO lo que necesite hacer para crear la lista correspondiente al pedido
-        if(pedido.getTipoLimpieza()){
-
+        for (Robot r: robots ) {
+            if( pedido.getTipoLimpieza().equals(r.getTipoLimpieza())){
+                robotsLimpieza.add(r);
+            }
         }
 
+        for (Robot r: robots ) {
+            if( pedido.isOrdenamiento() && r.isOrdena()){
+                robotsOrdenamiento.add(r);
+            }
+        }
 
-        return robots;
+        //TODO analizar las 2 listas para ver cuales son los robots mas economicos que cumplan con el pedido
+
+
+
+
+        return robotsAsignados;
     }
 
 }
