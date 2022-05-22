3390d9029c4b4cf777106ef8c3a62d97d87b6495
jgabrielc84 <jgabrielc84@gmail.com>
2022-05-21 21:13:41 -0300

    metodos ValidadorPedido

--- src/main/java/ar/edu/cuvl/validator/ValidadorPedido.java
+++ src/main/java/ar/edu/cuvl/validator/ValidadorPedido.java
@@ -2,13 +2,27 @@ package ar.edu.cuvl.validator;
 
 import ar.edu.cuvl.exception.PedidoInvalidoException;
 import ar.edu.cuvl.model.Pedido;
+import ar.edu.cuvl.model.TipoLimpieza;
+import ar.edu.cuvl.model.TipoSuperficie;
+import ar.edu.cuvl.model.type.Limpieza;
+
+import java.util.ArrayList;
 
 public class ValidadorPedido {
 
+    private String mensaje;
+
     public void validarEsPedido(Pedido pedido) throws PedidoInvalidoException {
 
-        if(true){ //TODO Validar que sea un pedido
-           throw new PedidoInvalidoException("Pedido invalidado");
+        try{
+            validarNumeroPedido(pedido);
+            validarNumeroCliente(pedido);
+            validarDireccion(pedido);
+            validarTipoLimpieza(pedido);
+            validarOrdenamiento(pedido);
+            validarSuperficies(pedido);
+        }catch (Exception e){
+           throw new PedidoInvalidoException(mensaje);
         }
     }
 
@@ -17,6 +31,49 @@ public class ValidadorPedido {
         if(true){ //TODO Validar que el pedido se puede procesar
             throw new PedidoInvalidoException("Pedido invalidado");
         }
+    }
 
+    private void validarNumeroPedido(Pedido pedido) throws Exception {
+
+        if(pedido.getNumeroPedido() <= 0){
+            this.mensaje = "Numero de pedido invalido";
+            throw new Exception();
+        }
+    }
+
+    private void validarNumeroCliente(Pedido pedido) throws Exception{
+
+        if(pedido.getNumeroCliente() <= 0){
+            this.mensaje = "Numero de cliente invalido";
+            throw new Exception();
+        }
+    }
+
+    private void validarDireccion(Pedido pedido) throws Exception{
+
+        if(pedido.getDireccion().length() == 0){
+            this.mensaje = "Direccion Invalida";
+            throw new Exception();
+        }
+    }
+
+    private void validarTipoLimpieza(Pedido pedido) throws Exception{
+        //TODO decidir si TIPOLIMPEZA tiene sentido de existir
+        if(pedido.getTipoLimpieza().equals(Limpieza.SIMPLE) || pedido.getTipoLimpieza().equals(Limpieza.COMPLEJA)){
+            this.mensaje = "Tipo de limplieza invalido";
+            throw new Exception();
+        }
+    }
+
+    private void validarOrdenamiento(Pedido pedido) throws Exception{
+
+        if(pedido.isOrdenamiento() != true || pedido.isOrdenamiento() != false){
+            this.mensaje = "Ordenamiento invalido";
+            throw new Exception();
+        }
+    }
+
+    private void validarSuperficies(Pedido pedido) throws Exception{
+        //TODO ver como validar una Direccion
     }
 }
