b9e34bb02fdfd3af30c2c3800884c5c5bac32490
Kenny <turicsa@gmail.com>
2022-06-28 19:14:14 -0300

    Separacion FactoryTipoReparacionGas/FactoryTipoReparacionElectricidad

--- src/main/java/ar/edu/cuvl/interfaces/Calculable.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.cuvl.interfaces;
-
-import ar.edu.cuvl.model.Empleado;
-import ar.edu.cuvl.model.ServicioReparacion;
-
-public interface Calculable {
-
-    public float calcularPrecioFinal();
-}
